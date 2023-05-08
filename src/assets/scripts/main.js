e3b8d39f7fb645585fbfed5810b3d7921dbdf963
Alejandro Gonzalez Hernandez <agonzalezhernan@uoc.edu>
2023-05-08 19:42:50 +0200

    Primera aproximación a estilos con Bootstrap. Ajustes en portada, miembros y noticias.

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,14 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
